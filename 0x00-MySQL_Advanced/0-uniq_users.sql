31da60a8b465738f36b285590cbf52d93ff107c9
Salma-fathi <salma89040@gmail.com>
2024-10-17 22:48:42 +0400

    creat table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,10 @@
--- Creates a table with unique users.
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- Script to create a 'users' table
+-- It includes id, email (unique), and name fields
+-- If the table already exists, the script will not fail
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
