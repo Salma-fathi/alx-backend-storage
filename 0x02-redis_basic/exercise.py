42dc557f546ac5e9feff87e70f800d266569a4d9
Salma-fathi <salma89040@gmail.com>
2024-10-23 22:03:49 +0400

    that takes a key string argument and an optional Callable

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,7 +18,23 @@ _redis (using redis.Redis()) and flush the instance using flushdb."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Method that takes a key string argument and an optional Callable argument named fn.
+        This callable will be used to convert the data back to the desired format."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+    def get_int(self, key: str) -> int:
+        """Method that takes a key string argument and returns an integer."""
+        data = self._redis.get(key)
+        return int(data)
+    def get_str(self, key: str) -> str:
+        """Method that takes a key string argument and returns a string."""
+        data = self._redis.get(key)
+        return str(data)
 if __name__ == "__main__":
     cache = Cache()
-    print(cache.store("Hello hbtn!"))
-    
+    print(cache.store("Hello World"))
+    print(cache.store(1024))
+    print(cache.store(3.14))
